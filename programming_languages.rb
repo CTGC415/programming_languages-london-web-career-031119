175d9a7d8a0afbbfbeee0a7552bd86d600703b48
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-05 12:03:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
   languages.each do |styles, info|
     info.each do |name, data|
       binding.pry
-      new_hash[name] = {data, :style = []}
+      new_hash[name] = {data}
       new_hash[name][:style] << styles
     end
   end
